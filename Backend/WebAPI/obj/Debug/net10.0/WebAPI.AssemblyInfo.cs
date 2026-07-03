e4aa048369d8931d10bf0e92ef790e379cdb1434
zdkazuha <aartemchelyk@gmail.com>
2026-07-03 04:30:28 +0300

    Fix: remove Project/obj from git and update gitignore

--- Backend/WebAPI/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
+++ Backend/WebAPI/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1f26465b473c134667527e8031f4f61b46f8059")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfd640370465634cdfc88b7c8b1f9dd5734e7505")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
