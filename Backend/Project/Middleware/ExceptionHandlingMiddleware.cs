870e489ba16f2e44afd9ca9ec82c70b9501c452f
Chelyk Artem <aartemchelyk@gmail.com>
2026-07-14 02:26:31 +0300

    Architecture/serilog and middleware (#2)
    * Initial commit
    
    * add serilog and middleware
    
    * delete git garbage
    
    * Fix: remove Project/obj from git and update gitignore
    
    * update

---
+++ Backend/Project/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿namespace Domain;
+using System.Net;
+using System.Text.Json;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        _logger.LogError(exception, "An unexpected error occurred in the Sonara application");
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        var response = new
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = "Oops, something went wrong on the Sonara server. We are already working on it!",
+            Detailed = exception.Message
+        };
+
+        var jsonResponse = JsonSerializer.Serialize(response);
+        return context.Response.WriteAsync(jsonResponse);
+    }
+}
