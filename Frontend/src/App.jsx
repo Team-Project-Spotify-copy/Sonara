449812d2141e664e8b5e975a2537be00d5bc5186
zdkazuha <aartemchelyk@gmail.com>
2026-07-24 10:56:50 +0300

    add pages(login, register, reset password, create new password)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './components/Home.jsx'
+import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
+import CreateNewPassword from "./components/CreateNewPassword.jsx";
+import ResetPassword from "./components/ResetPassword.jsx";
+import "./index.css";
 import './App.css'
-
 function App() {
 
   return (
@@ -9,10 +13,14 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/create-new-password" element={<CreateNewPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
---
+++ Frontend/src/components/Login.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import image from "../assets/images/login-bg.png";
+import "../css/Login.css"; 
+
+function Login() {
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <div className="login-intro">
+          <div className="login-avatar"></div>
+
+          <h1 className="login-title">Welcome Back!</h1>
+
+          <div className="login-form-wrapper">
+            <form className="login-form">
+              <div className="form-group">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  required
+                  className="form-input"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  required
+                  className="form-input"
+                />
+              </div>
+
+              <button type="submit" className="btn-primary">
+                Continue
+              </button>
+            </form>
+
+            <p className="login-divider">or</p>
+
+            <div className="login-social-group">
+              <button type="button" className="btn-social">
+                Google
+              </button>
+              <button type="button" className="btn-social">
+                Facebook
+              </button>
+            </div>
+
+            <p className="login-footer-text">
+              Don't have an account?{" "}
+              <a href="/register" className="app-link">
+                Sign up!
+              </a>
+              <br />
+              <a
+                href="/forgot-password"
+                className="app-link"
+                style={{ fontWeight: "normal" }}
+              >
+                Forgot your password?
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="login-bg-image">
+        <img src={image} alt="Login Background" />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
