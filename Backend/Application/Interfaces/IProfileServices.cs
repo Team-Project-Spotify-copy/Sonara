b5b20d541d9fd4d213877d2da581db96621dd483
Chelyk Artem <aartemchelyk@gmail.com>
2026-08-11 22:29:27 +0300

    add profile controler (#17)

---
+++ Backend/Application/Interfaces/IProfileServices.cs
@@ -0,0 +1,17 @@
+﻿using Application.DTOs.Users;
+using Domain.Entities.Users;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Interfaces;
+
+public interface IProfileServices
+{
+    Task<ProfileDto> GetProfileAsync(Guid userId);
+
+    Task<ProfileDto> GetUserByUsernameAsync(string username);
+
+    Task<ProfileDto> UpdateProfileAsync(Guid userId, UpdateProfileDto profileDto);
+
+    Task<bool> SetAvatarAsync(Guid userId, IFormFile avatarFile);
+
+}
